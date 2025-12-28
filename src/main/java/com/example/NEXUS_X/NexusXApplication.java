34459c89d76e36c17513fd68e47bc5002688829e
tchunnuji7 <tchunnuji7@gmail.com>
2025-12-28 12:06:42 +0530

    Spring boot app initilised, db conf done

---
+++ src/main/java/com/example/NEXUS_X/NexusXApplication.java
@@ -0,0 +1,13 @@
+package com.example.NEXUS_X;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusXApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NEXUS_X/NexusXApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NEXUS_X;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
